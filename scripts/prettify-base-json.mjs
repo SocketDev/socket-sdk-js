8050855763b4baf40343713656ce4867aa5b7678
jdalton <john.david.dalton@gmail.com>
2025-09-18 10:18:36 -0400

    Update fileURLToPath references

--- scripts/prettify-base-json.mjs
+++ scripts/prettify-base-json.mjs
@@ -1,8 +1,8 @@
 import fs from 'node:fs/promises'
 import path from 'node:path'
-import url from 'node:url'
+import { fileURLToPath } from 'node:url'
 
-const __filename = url.fileURLToPath(import.meta.url)
+const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const rootPath = path.join(__dirname, '..')
